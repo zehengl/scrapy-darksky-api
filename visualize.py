8c9265f2de8e63377d78e65f64269bd094f03b6a
Zeheng Li <imzehengl@gmail.com>
2019-10-10 10:41:22 -0600

    revamp plots

--- visualize.py
+++ visualize.py
@@ -1,5 +1,6 @@
 #%%
 from datetime import datetime
+from itertools import combinations
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -13,48 +14,63 @@ result.mkdir(exist_ok=True)
 df = pd.concat(map(pd.read_json, path.glob("forcast_*.json")), ignore_index=True)
 
 df["time"] = df.apply(lambda r: datetime.fromtimestamp(r["time"]), axis=1)
+df["precip_type"] = df.apply(lambda r: r["precip_type"] or "none", axis=1)
+
+cat_attrs = ["icon", "precip_type"]
+num_attrs = [
+    "cloud_cover",
+    "dew_point",
+    "humidity",
+    "precip_accumulation",
+    "precip_intensity",
+    "precip_probability",
+    "pressure",
+    "temperature",
+    "visibility",
+    "wind_bearing",
+    "wind_gust",
+    "wind_speed",
+]
 
 
 #%%
-plt.clf()
-ax = sns.countplot(x="icon", data=df)
-ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
-for p in ax.patches:
-    height = p.get_height()
-    ax.text(
-        p.get_x() + p.get_width() / 2.0,
-        height,
-        f"{height/ df.shape[0] * 100:.2f}%",
-        ha="center",
+for x in cat_attrs:
+    plt.clf()
+    ax = sns.countplot(x=x, data=df)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
+    for p in ax.patches:
+        height = p.get_height()
+        ax.text(
+            p.get_x() + p.get_width() / 2.0,
+            height,
+            f"{height/ df.shape[0] * 100:.2f}%",
+            ha="center",
+        )
+    ax.get_figure().savefig(result / f"countplot-{x}.png", bbox_inches="tight")
+
+
+#%%
+for y in num_attrs + cat_attrs:
+    plt.clf()
+    ax = sns.scatterplot(x="time", y=y, s=4, data=df)
+    ax.set(xlim=(min(df["time"]), max(df["time"])))
+    num_ticks = len(ax.get_xticklabels())
+    date_range = pd.date_range(min(df["time"]), max(df["time"]), num_ticks + 2)
+    xtick_labels = [d.strftime("%y-%m-%d") for d in date_range[1:-1]]
+    ax.set_xticklabels(xtick_labels, rotation=40, ha="right", fontsize=7)
+    ax.get_figure().savefig(
+        result / f"scatterplot-{y}-over-time.png", bbox_inches="tight"
     )
-ax.get_figure().savefig(result / "icon-countplot.png", bbox_inches="tight")
+
+
+#%%
+for x, y in combinations(num_attrs, 2):
+    plt.clf()
+    ax = sns.jointplot(x=x, y=y, kind="hex", data=df)
+    ax.fig.savefig(result / f"jointplot-{x}-{y}.png")
 
 
 #%%
 plt.clf()
-ax = sns.scatterplot(x="time", y="temperature", s=4, data=df)
-ax.set(xlim=(min(df["time"]), max(df["time"])))
-ax.get_figure().savefig(result / "temperature-scatterplot.png")
-
-
-#%%
-plt.clf()
-ax = sns.scatterplot(x="time", y="pressure", s=4, data=df)
-ax.set(xlim=(min(df["time"]), max(df["time"])))
-ax.get_figure().savefig(result / "pressure-scatterplot.png")
-
-
-#%%
-plt.clf()
-ax = sns.jointplot(x="temperature", y="pressure", kind="hex", data=df)
-ax.fig.savefig(result / "temperature-pressure-jointplot.png")
-
-
-#%%
-plt.clf()
-ax = sns.pairplot(
-    vars=["wind_speed", "wind_gust", "humidity", "pressure", "temperature"],
-    plot_kws={"s": 4},
-    data=df,
-)
+ax = sns.pairplot(vars=num_attrs, plot_kws={"s": 4}, data=df)
 ax.fig.savefig(result / "pairplot.png")
